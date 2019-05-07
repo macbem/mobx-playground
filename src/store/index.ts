31dfddd9aa2d137789afb962e09c266fc4726b08
Maciej Bembenista <maciej.bembenista@gmail.com>
2019-05-07 23:32:17 +0200

    Disallow changes outside @action

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,5 @@
+import { configure } from 'mobx';
+
+configure({ enforceActions: 'observed' }); // don't allow state modifications outside actions
+
 export { rootStoreInstance, RootStore } from './root-store.store';
